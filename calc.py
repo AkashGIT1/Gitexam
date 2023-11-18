3409079358d15048329c998fc2bebf6fc9a3ac59
Akash <akashkvs91@gmail.com>
2023-11-18 10:53:32 +0530

    new commit

--- calc.py
+++ calc.py
@@ -1,3 +1,10 @@
+def add(a,b):
+    print(a-b)
+a=int(input("enter a")
+b=int(input("enter b")
+op=input("enter operation + - * or / : ")
+if op=='+':
+      add(a,b)
 def multiply(a,b):
     print(a*b)
 def diivide(a,b):
@@ -6,3 +13,7 @@ if op=='*':
     multiply(a,b)
 if op=='/':
     divide(a,b)
+[A[A[A[A[A[A[A[A[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[+[C[C
+[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[C[Ddef sub(a,b):
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C
+    print(a-b)
